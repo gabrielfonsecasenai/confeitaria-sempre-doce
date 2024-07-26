169ffeee785e3c589c0a5db98976758fd2fa999d
gabrielfonsecasenai <140816757+gabrielfonsecasenai@users.noreply.github.com>
2024-07-26 10:08:17 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def inicial():
     return render_template("index.html")
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
 
 if __name__ == '__main__':
     app.run()
